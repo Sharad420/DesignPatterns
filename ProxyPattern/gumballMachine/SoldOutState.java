cde90b7d6ff891f87f64fb2ffc8e0c852d8ffa74
Sharad420 <79397589+Sharad420@users.noreply.github.com>
2025-04-21 23:38:53 +0530

    Add files via upload

---
+++ ProxyPattern/gumballMachine/SoldOutState.java
@@ -0,0 +1,37 @@
+public class SoldOutState implements State {
+    private static final long serialVersionUID = 2L;
+    transient GumballMachine gumballMachine;
+
+    public SoldOutState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    // Implement the state interface methods.
+    public void insertQuarter() {
+        System.out.println("You inserted a quarter, but the machine is sold out!");
+    }
+
+    public void ejectQuarter() {
+        System.out.println("You haven't inserted a quarter!");
+    }
+
+    public void turnCrank() {
+        System.out.println("You need to insert a quarter first! Also, the machine is sold out!");
+    }
+
+    public void dispense() {
+        System.out.println("No gumball dispensed, the machine is sold out!");
+    }
+
+    public void refill(int count) {
+        System.out.println("Refilling gumball machine with " + count + " gumballs");
+        gumballMachine.setCount(gumballMachine.getCount() + count);
+        if (gumballMachine.getCount() > 0) {
+            gumballMachine.setState(gumballMachine.getNoQuarterState());
+        } else {
+            System.out.println("The machine is still sold out!");
+            gumballMachine.setState(gumballMachine.getSoldOutState());
+        }
+    }
+
+}
